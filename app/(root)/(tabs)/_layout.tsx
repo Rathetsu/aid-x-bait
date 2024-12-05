63c0077184165d5e9e85871dbad603df19d8a67a
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2024-12-05 03:38:44 +0200

    bottom navbar

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -10,19 +10,13 @@ const TabIcon = ({
 	focused: boolean;
 	source: ImageSourcePropType;
 }) => (
-	<View
-		className={`flex flex-row justify-center items-center rounded-full ${focused ? "bg-general-300" : ""}`}
-	>
-		<View
-			className={`rounded-full w-12 h-12 justify-center items-center ${focused ? "bg-general-450" : ""}`}
-		>
-			<Image
-				source={source}
-				tintColor="white"
-				resizeMode="contain"
-				className="w-7 h-7"
-			/>
-		</View>
+	<View className="flex justify-center items-center">
+		<Image
+			source={source}
+			tintColor={focused ? "#49869F" : "#888"}
+			resizeMode="contain"
+			className="w-8 h-8"
+		/>
 	</View>
 );
 
@@ -31,22 +25,21 @@ const Layout = () => {
 		<Tabs
 			initialRouteName="home"
 			screenOptions={{
-				tabBarActiveTintColor: "white",
-				tabBarInactiveTintColor: "white",
-				tabBarShowLabel: false,
+				tabBarActiveTintColor: "#8888",
+				tabBarInactiveTintColor: "#888",
+				tabBarShowLabel: true,
 				tabBarStyle: {
-					backgroundColor: "#333333",
-					borderRadius: 50,
-					paddingBottom: 0,
-					overflow: "hidden",
-					marginHorizontal: 20,
-					marginBottom: 20,
-					height: 80,
+					backgroundColor: "#ffffff",
+					borderRadius: 10,
+					height: 90,
 					display: "flex",
-					justifyContent: "space-between",
-					alignItems: "center",
-					flexDirection: "row",
 					position: "absolute",
+					paddingTop: 10,
+					shadowColor: "#000",
+					shadowOffset: { width: 0, height: 5 },
+					shadowOpacity: 0.1,
+					shadowRadius: 10,
+					elevation: 5,
 				},
 			}}
 		>
