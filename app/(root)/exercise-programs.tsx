4ed50507d33212adb61552a48ce2e7ebcb143c53
mohammadghulmi <mohammad.ghulmi@gmail.com>
2025-01-27 14:35:37 +0200

    fix

--- app/(root)/exercise-programs.tsx
+++ app/(root)/exercise-programs.tsx
@@ -1,12 +1,15 @@
 import InteractiveSkeleton from "@/components/Exercises/InteractiveSkeleton/InteractiveSkeleton";
 import { Text } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const ExercisePrograms = () => {
 	return (
+		<GestureHandlerRootView>
 		<SafeAreaView>
 			<InteractiveSkeleton />
 		</SafeAreaView>
+		</GestureHandlerRootView>
 	);
 };
 
